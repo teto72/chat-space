4791df8686d82be9c5ea961f5f363dd49df0d3ed
teto72 <tsubasa.k.729@gmail.com>
2020-04-26 15:12:51 +0900

    ユーザ管理実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+end
+
+protected_methods
+
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up,keys: [:nickname])
 end
