3abf915aacd63734ed849fbccbb0edf10579a52b
teto72 <tsubasa.k.729@gmail.com>
2020-05-04 19:13:40 +0900

    自動デプロイ修正2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '18.180.186.77', user: 'ec2-user', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -59,4 +61,3 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '18.180.186.77', user: 'ec2-user', roles: %w{app db web}
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,14 +1,12 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1
-
-#アプリケーションの設置されているディレクトリを指定
+# currentを指定
 working_directory "#{app_path}/current"
 
-#Unicornの起動に必要なファイルの設置場所を指定
-
+# それぞれ、sharedの中を参照するよう変更
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
